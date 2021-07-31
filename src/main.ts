d4c536d8394e4b85a9b282468f502370e86ce7b3
Andrew Sims <andrew.sims@weployapp.com>
2021-07-31 10:16:22 +1000

    V1

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import RuleSet from './RuleSet'
+import Round from './Round'
+import Dice from './dice/Dice'
+import DiceFactory from './dice/DiceFactory'
+import { standardRules } from './rules'
+
+export { RuleSet, Round, DiceFactory, Dice, standardRules }
